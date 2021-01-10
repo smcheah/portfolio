b7370218cbaf276470114fb2f3a79fa40e633298
Simone Cheah <simone_cheah@hotmail.com>
2021-01-11 03:37:02 +1100

    added no match page

---
+++ src/pages/NoMatch/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Card from "../../components/Card";
+// import "./style.css";
+
+function NoMatch () {
+    return <Card>
+        <h1>404: PAGE NOT FOUND :(</h1>
+    </Card>;
+};
+
+export default NoMatch;
