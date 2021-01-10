795c03e0c15f95c0a3b0a468bcfd787c9562cd8f
Simone Cheah <simone_cheah@hotmail.com>
2021-01-11 03:58:18 +1100

    update to router paths

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import NoMatch from "./pages/NoMatch";
+import Home from "./pages/Home";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App () {
+  return <>
+    <Header />
+    <main>
+      <Router>
+        <Switch>
+
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/showcase" >
+
+          </Route>
+          <Route exact path="/showcase/:project" >
+
+          </Route>
+          <Route>
+            <NoMatch />
+          </Route>
+
+        </Switch>
+      </Router>
+    </main>
+    <Footer />
+  </>;
 }
 
 export default App;
